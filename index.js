78e37faefa04d6267d2fb5365907f948fd43ddab
Matheus Catossi <mcatossi@br.ibm.com>
2019-06-08 21:17:32 -0300

    add node server

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 let ffmpeg = require('fluent-ffmpeg');
 let VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 let fs = require('fs');
+const express = require('express');
+const app = express();
+const Promise = require('bluebird');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({
+    uploadDir: './assets'
 
-let params = {
+});
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-main(params).then(result => { })
+app.use(cors("*"));
 
-function main(params) {
-    return new Promise(function (resolve, reject) {
+app.post('/api/upload', multipartMiddleware, (req, res) => {
+    fs.rename('./' + req.files.uploads[0].path, './assets/' + 'video.mp4', function (err) {
+        if (err) throw err;
+        fs.stat('./assets/' + 'video.mp4', function (err, stats) {
+            if (err) throw err;
+            console.log('stats: ' + JSON.stringify(stats));
+        });
+    });
 
-
-        ffmpeg('./assets/video_example.mp4')
-            .on('end', function () {
-                console.log('Screenshots taken');
-            })
-            .on('error', function (err) {
-                console.error(err);
-            })
-            .screenshots({
-                // Will take screenshots at 20%, 40%, 60% and 80% of the video
-                count: 10,
-                folder: './assets/'
-            });
+    res.json({
+        'message': 'File uploaded succesfully.'
+    });
+});
 
 
+
+
+app.get('/api/verify', multipartMiddleware, (req, res) => {
+    console.log("GET /api/verify");
+
+    cropVideo().then(function () {
         let listRequest = [];
         for (let index = 1; index <= 10; index++) {
             listRequest.push({
@@ -38,23 +50,57 @@ function main(params) {
             request
         })))).then(results => {
             let countScore = 0;
-            for(let result of results) {
-                if(result.request.images[0]){
-                    if(result.request.images[0].classifiers[0]) {
-                        if(result.request.images[0].classifiers[0].classes[0]) {
-                            if(result.request.images[0].classifiers[0].classes[0].score != undefined) {
-                                if(result.request.images[0].classifiers[0].classes[0].score > 0.75) {
-                                    console.log(result.request.images[0].classifiers[0].classes[0].score);
-                                    countScore++;
+            for (let result of results) {
+                if (result.request.images[0]) {
+                    if (result.request.images[0].classifiers[0]) {
+                        if (result.request.images[0].classifiers[0].classes[0]) {
+                            if (result.request.images[0].classifiers[0].classes[0].score != undefined) {
+                                let score = result.request.images[0].classifiers[0].classes[0].score;
+                                if (score > 0.75 && score <= 0.85) {
+                                    countScore += 1;
+                                } else if (score > 0.85 && score <= 0.90) {
+                                    countScore += 2;
+                                } else if (score > 0.90) {
+                                    countScore += 3;
                                 }
+                                console.log(score);
                             }
                         }
                     }
                 }
             }
-            console.log(countScore);
-        });
 
+            console.log(countScore);
+            if (countScore > 3) {
+                console.log("Você acertou o comando");
+                res.send({
+                    result: "Você acertou o comando"
+                })
+            } else {
+                console.log("Você errou o comando");
+                res.send({
+                    result: "Você errou o comando"
+                })
+            }
+        });
+    });
+
+});
+
+function cropVideo() {
+    return new Promise(function (resolve, reject) {
+        ffmpeg('./assets/video_example_6.mp4')
+            .on('end', function () {
+                console.log('Screenshots taken');
+                resolve(true);
+            })
+            .on('error', function (err) {
+                console.error(err);
+            })
+            .screenshots({
+                count: 10,
+                folder: './assets/'
+            });
     });
 }
 
@@ -65,7 +111,6 @@ function requestVisual(index) {
             version: '2018-03-19',
             iam_apikey: 'EFC2KiU_nWKRHNuXCgNGrhYOxk2NeLBeZNSlQuwveb82'
         });
-
 
         var images_file = fs.createReadStream("./assets/tn_" + index + ".png");
         var classifier_ids = ["DefaultCustomModel_916556356"];
@@ -82,6 +127,7 @@ function requestVisual(index) {
                 console.log(err);
                 resolve(JSON.stringify(response, null, 2));
             } else {
+                console.log("Request VisualRecognition Success!")
                 resolve(response);
             }
         });
@@ -89,3 +135,8 @@ function requestVisual(index) {
 }
 
 
+
+var port = process.env.PORT || 3001
+app.listen(port, function () {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
